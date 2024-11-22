60670aa83f4074dd2bd72340f6d1abb6953a2ca9
MarianaLGM <mariana_lobeto@hotmail.com>
2024-11-22 12:33:01 +0100

    cambios

--- js/script.js
+++ js/script.js
@@ -1,116 +1,61 @@
 
-const userInput= document.getElementById("userInput") //desplegable donde elegimos número 1 al 3
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const userInput= document.getElementById("userInput"); //desplegable donde elegimos número 1 al 3
 const countdown= document.getElementById("countdown");//cuenta atrás (5 segun) 5,4,3,2,1,0
 let result= document.getElementById("result"); //"¡Has salvado el mundo!" ó "La bomba ha estallado"  
-let resultados=document.getElementById("contenedorResulados");
+let restart = document.getElementById("restart");
+
 let eleccionUsuario=0;
 
-// comparar seleccion usuario vs ordenador cuando haga click:
+//seleccion usuario vs ordenador cuando haga click:
 
 userInput.addEventListener("change", function() { //el change es para los desplegables cuando cambian de opción y se escoge entre varios.Aqui podría usar el keydown para que cuando presione enter o una tecla 
-    eleccionUsuario = userInput.value
-  })
+  eleccionUsuario = userInput.value
+})
 
 const eleccionOrdenador = Math.floor(Math.random()*3) + 1  //numero aleatorio selección ordenador
 
-console.log("la eleccion del ordenador es",eleccionOrdenador) //ok
-console.log("mi eleccion es",eleccionUsuario)//ok
 
-
-/*Función para comenzar la cuenta atrás- setInterval*/
-
-function cuentaAtras() {
-    if (eleccionUsuario==0) {
-        eleccionUsuario=setInterval(5,4,3,2,1,0);
-      console.log(cuentaAtras)
-  
-  countdown.addEventListener("click", cuentaAtras)
-  }
-} 
-
-
-// Primero obten el selector donde caerá el número
-const cont = document.getElementById('cont');
-
-/*Función comenzar juego
-     - Con una cuenta atrás (esto será otra función que debes crear y luego la llamas aquí)
-     - promesa para crear el número random del ordenador:
-           - con un setTimeout para que se resuelva en el mismo tiemo que la cuenta atrás.
-     - Debe retornar el eleccionOrdenador.*/
-     function inicioJuego(){   
-     const promesa1 = new Promise ((resolve) =>{
-       setTimeout(()=>{
-         cuentaAtras= "0"
-         resolve (cuentaAtras)
-        },5000);
-       });
-       console.log (promesa1)
-     const promesa2 = new Promise ((resolve) =>{
-       setTimeout(()=>{
-         resolve (eleccionOrdenador)
-         },5000);
-       });
-       console.log (promesa2) 
-     
-    promesa1
-     .then ((cuentaAtras)=>
-     console.log(cuentaAtras)
-     );
-     promesa2
-     .then ((eleccionOrdenador)=>
-     console.log(eleccionOrdenador)
-     );
-     
-     const sumaPromesas=Promise.all([promesa1, promesa2]).then((values) => {
-       console.log(promesa1,promesa2)
-     })
-     
-     
-    }
-
-
-
-     /*
-function inicioJuego(){
-    return new Promise((resolve, reject) => { //resolve=resuelto y reject= rechazado (error)
-    if (eleccionOrdenador.length===0) {
-        reject (new error("Inicio del juego is empty"))
-    }
-        setTimeout (() =>{
-        resolve (eleccionOrdenador, cuentaAtras);
-    }, 5000)
-   
-    .then((response) => console.log (response))
-    .catch((err) => console.log(err.mensaje))  
-    })
-    
-} */
- 
-
-/*Ejecutamos la función "Comenzar juego" con una promesa .then
-y una condicional donde le diremos que si el numero seleccionado por el ordenador (random) es igual que el número seleccionado por el usuario ...
-Esto es lo que ya te pone en el enunciado de las frases.*/
-
-function resultadoJuego(){
-    
-   document.createElement('p')=result;
-   resultados.appenchild (result);
-
-    if (eleccionUsuario===eleccionOrdenador){
-       result= "¡Has salvado el mundo! Tu número " + eleccionUsuario + " es el mismo que el número " + eleccionOrdenador;//le estoy diciento que en la varialble result me pinte esa frase
-   }else{
-       result="¡La bomba ha estallado! Tu número " + eleccionUsuario + " es diferente que el número " + eleccionOrdenador;//le estoy diciento que en la varialble result me pinte esa frase
-     }
-console.log(result)
+//función juego
+function inicioJuego() {
+  cuentaAtras(5);
+  const promesaNumero = new Promise((resolve) => {
+    setTimeout(() => {
+      const eleccionOrdenador = Math.floor(Math.random() * 3) + 1;
+      resolve(eleccionOrdenador);
+    }, 6000); //set interval son 6 vueltas (del 5 al 0)
+  });
+  return promesaNumero;
 }
-     
-/*
-//Para restablecer el juego -(evento y una acción) 
 
-    const restart= document.getElementById("restart").value.reset();//botón para resetear el juego
 
-    restart.addEventListener("click", function () {//resetar al hacer click
-     eleccionUsuario===0   
-})
 
-*/
+//funcion cuenta atrás
+function cuentaAtras(tiempo) {
+  if (eleccionUsuario==0) {
+    tiempo=setInterval(5,4,3,2,1,0);
+    setTimeout(() => {
+countdown.addEventListener("click", cuentaAtras)
+
+//mensajeCountdown = `<h3> Cuenta atrás: ${tiempo} segundos</h3>`;
+ //countdown.innerHTML=mensajeCountdown;
+
+  },6000)
+}
+}
+
+
+inicioJuego().then((numOrdenador) => {
+  //aquí el condicional donde se compara la eleccion del usuario y la del ordenador y donde se mostrará el mensaje según resultado)
+  if (eleccionUsuario === numOrdenador) {
+    mensaje = `<h3> ¡Has salvado el mundo!Tu número ${eleccionUsuario} es el mismo que el número ${numOrdenador}</h3>`; //le estoy diciento que en la varialble result me pinte esa frase
+  } else {
+    mensaje = `<h3>¡La bomba ha estallado! Tu número ${eleccionUsuario} es diferente que el número ${numOrdenador}</h3>`; //le estoy diciento que en la varialble result me pinte esa frase
+  }
+  //poner el mensaje en el div id"result"
+  result.innerHTML = mensaje;
+});
+restart.addEventListener("click", function () {
+  location.reload();
+});
