188b618339dc63d3f49d97c2c08e214f3e163ab8
MarianaLGM <mariana_lobeto@hotmail.com>
2024-11-26 11:20:20 +0100

    añado comentario

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 const userInput= document.getElementById("userInput"); //desplegable donde elegimos número 1 al 3
-const countdown= document.getElementById("countdown");//cuenta atrás (5 segun) 5,4,3,2,1,0
+
 let result= document.getElementById("result"); //"¡Has salvado el mundo!" ó "La bomba ha estallado"  
 let restart = document.getElementById("restart");
 
@@ -18,6 +18,7 @@ const eleccionOrdenador = Math.floor(Math.random()*3) + 1  //numero aleatorio se
 
 
 //función juego
+const countdown= document.getElementById("countdown");//cuenta atrás (5 segun) 5,4,3,2,1,0
 function inicioJuego() {
   cuentaAtras(5);
   const promesaNumero = new Promise((resolve) => {
@@ -29,24 +30,22 @@ function inicioJuego() {
   return promesaNumero;
 }
 
-
 //funcion cuenta atrás// NO VUELCA EL CONTADOR EN WEB 5,4,3,2,1,0
 function cuentaAtras(tiempo) {
   if (eleccionUsuario==0) {
     tiempo=setInterval(5,4,3,2,1,0);
     setTimeout(() => {
 countdown.addEventListener("click", cuentaAtras)
-
   },6000)
+  //countdown.innerHTML=("Cuenta atrás: "+ tiempo, "segundos")
+} 
 }
-}
-
 
 
 inicioJuego().then((numOrdenador) => {
   //condicional que compara la eleccion del usuario y la del ordenador y donde se mostrará el mensaje según resultado)
   if (eleccionUsuario == 1 & numOrdenador== 1) {
-    mensaje = `<h3>¡Has salvado el mundo!Tu número ${eleccionUsuario} es el mismo que el número ${numOrdenador}</h3>`; //le estoy diciento que en la varialble result me pinte esa frase
+    mensaje = `<h3>¡Has salvado el mundo!Tu número ${eleccionUsuario} es el mismo que el número ${numOrdenador}</h3>`; 
   }else if (eleccionUsuario == 2 & numOrdenador== 2){
     mensaje = `<h3>¡Has salvado el mundo!Tu número ${eleccionUsuario} es el mismo que el número ${numOrdenador}</h3>`;
   }else if (eleccionUsuario == 3 & numOrdenador== 3){
@@ -58,7 +57,7 @@ inicioJuego().then((numOrdenador) => {
   //poner el mensaje en el div id"result"
   result.innerHTML = mensaje;
 });
-restart.addEventListener("click", function () {
+restart.addEventListener("click", function () {//reinciar juego
   location.reload();
 });
 
